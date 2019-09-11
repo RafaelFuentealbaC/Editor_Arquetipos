b2323d2be667a5188289aa57064fa1667ef75aaf
Nicol <nicol.orellana2015@alu.uct.cl>
2019-09-11 13:48:46 -0300

    Componentes

--- AppEditor/editor/src/main.js
+++ AppEditor/editor/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
 
 new Vue({
   router,
