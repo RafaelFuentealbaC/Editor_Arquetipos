40f5077e9f6fcb99fc0b7cef429e673e56edf346
Nicol <nicol.orellana2015@alu.uct.cl>
2019-09-20 13:01:38 -0300

    importar

--- AppEditor/editor/src/views/Importar.vue
+++ AppEditor/editor/src/views/Importar.vue
@@ -1,11 +1,30 @@
 <template>
     <div class="importar">
-        <h1>Importar arquetipo</h1>
+    <h1>Importar arquetipo</h1>
+    <div role="form">
+        <form>
+            <div class="form-group">
+                <label class="btn btn-primary">
+                    <i class="fa fa-folder-open-o" aria-hidden="true"></i>&nbsp;Seleccionar un archivo
+                    <input type="file" accept=".pdf" @change="onFileSelected" name="myfile">
+                </label>
+            </div>
+        </form>
+    </div>
+
+
+
+
+
+        
     </div>
 </template>
 
 <script>
+
+
 export default {
+    
     
 }
 </script>
