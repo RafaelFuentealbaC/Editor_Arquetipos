4d3f3e28139a4075ecfee5a9b0279909417080a1
Nicol <nicol.orellana2015@alu.uct.cl>
2019-09-24 23:37:29 -0300

    nuevo.vue

--- AppEditor/editor/src/views/Nuevo.vue
+++ AppEditor/editor/src/views/Nuevo.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="nuevo">
-        <h1>Nuevo arquetipo</h1>
-    </div>
+   
+    <div v-drag-and-drop:options="options" class="drag-wrapper">
+    <h1>Nuevo Arquetipo</h1>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <ul>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+    </ul>
+    <ul>
+      <li>Item 7</li>
+      <li>Item 8</li>
+      <li>Item 9</li>
+    </ul>
+  </div>
+  
 </template>
 
 <script>
