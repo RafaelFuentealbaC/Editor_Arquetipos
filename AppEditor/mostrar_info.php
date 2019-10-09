e59274d50015fa314b58ed1dc83de56a82a5a84f
Nicol <nicol.orellana2015@alu.uct.cl>
2019-10-09 12:06:36 -0300

    php que muestra informacion
    lee json

---
+++ AppEditor/mostrar_info.php
@@ -0,0 +1,49 @@
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <!-- Importa bootstrap, se usa ordenar los elementos -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Axios se usa para cargar la data json VueJS -->
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+  <!-- VueJS se usa para mostrar la informacion -->
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
+<body>
+  <div class="container mt-4" id="app">
+  <div class="row info-pannel">
+   
+<!-- Informacion basica -->
+    <div id="pac" class="col-sm-4">
+      <h1 v-for="user of users">{{  }}</h1>
+      <p v-for="user of users">Otro: {{ user.description.other_details }} </p> 
+      <p v-for="user of users">Nombre: {{ user.is_controlled }} </p> 
+      <p v-for="user of users">Descripcion: {{ user.is_controlled }} </p> 
+      <p v-for="user of users">Como utilizar: {{ user.is_controlled }} </p> 
+    </div>
+
+  </div>
+
+  </div>
+  <script>
+    var app = new Vue({
+      el: '#app', //Elemento de id app
+      data: {
+        users: [] //Se crea un arreglo con los datos recibidos de la estructura json
+      },
+      mounted: function() {
+        axios.get('data.json') //Busca una estructura json en el servidor
+            .then(response => {
+              this.users = response.data;
+              console.log(response);
+            })
+            .catch(error => { //Muestra error si la estructura no pudo cargarse
+              console.log(error);
+            });
+      }
+    })
+  </script>
+</body>
+</html>
